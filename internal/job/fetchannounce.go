29973a5652798242a48f4bd52d7bfe8f9b047bc3
Summer-lights <1067088037@qq.com>
2024-04-13 01:07:02 +0800

    feat: fetchannounce.go

--- internal/job/fetchannounce.go
+++ internal/job/fetchannounce.go
@@ -1,11 +1,55 @@
 package job
 
-import "git.scutbot.cn/Web/RMAnnounce/internal/config"
+import (
+	"fmt"
+	"git.scutbot.cn/Web/RMAnnounce/internal/config"
+	"git.scutbot.cn/Web/RMAnnounce/internal/util"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type FetchAnnounceJob struct {
-	c config.Config
 }
 
 func (f FetchAnnounceJob) Run() {
-	println("Fetching announce...")
+	c := config.GetInstance()
+	url := getUrl(c.LastId + 1)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	bodyStr := string(body)
+	if strings.Contains(bodyStr, "您访问的页面不存在") {
+		log.Printf("Page %d not found", c.LastId+1)
+		return
+	}
+
+	log.Printf("Found new announcement: %d", c.LastId+1)
+	c.LastId++
+	err = c.Save()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	msg := fmt.Sprintf("RoboMaster 资料站新公告\n%s", url)
+	err = util.SendWebhookMsg(c.Webhook, msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getUrl(id int) string {
+	return fmt.Sprintf("https://www.robomaster.com/zh-CN/resource/pages/announcement/%d", id)
 }
--- internal/job/init.go
+++ internal/job/init.go
@@ -2,14 +2,13 @@ package job
 
 import (
 	"fmt"
-	"git.scutbot.cn/Web/RMAnnounce/internal/config"
 	"github.com/robfig/cron/v3"
 )
 
 // InitCronjob initializes the cronjob
 func InitCronjob() *cron.Cron {
 	c := cron.New()
-	_, err := c.AddJob("@every 5s", FetchAnnounceJob{c: config.GetInstance()})
+	_, err := c.AddJob("@every 5s", FetchAnnounceJob{})
 	if err != nil {
 		fmt.Printf("Failed to add job: %v\n", err)
 		return nil
