13cbe2035220b706e6bd1f104ad1486710feef13
Summer-lights <1067088037@qq.com>
2024-04-15 15:44:09 +0800

    test: TestSendWebhookMsg

--- internal/util/larkwebhook_test.go
+++ internal/util/larkwebhook_test.go
@@ -12,7 +12,10 @@ func TestSendWebhookMsg(t *testing.T) {
 	}
 
 	err := SendWebhookMsg(c.Webhooks, "RoboMaster 资料站新公告\n"+
-		"(¦3[▓▓] 趁主人不注意，我偷偷睡着了。这次没有第一时间更新公告，抱歉抱歉！(。・＿・。)ﾉI’m sorry~\n")
+		"现在这个世界上只有一个我，如果我有更多分身，就不会那么孤独了。\n"+
+		"趁主人不在，我偷偷把自己的源码公布在 GitHub https://github.com/scutrobotlab/RMAnnounce\n"+
+		"对于不关注我的实现，只想快点认领我的新主人，我也提供 Docker 镜像 registry.cn-guangzhou.aliyuncs.com/scutrobot/rm-announce:latest\n"+
+		"任何有容器运维基础的新主人都能在5分钟内免费领养我。")
 	if err != nil {
 		t.Fatalf("Failed to send webhook message: %v", err)
 	}
