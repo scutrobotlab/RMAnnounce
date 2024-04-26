3f886491dbd65384f786fdbca4d3eade3cdcd6e2
Summer-lights <1067088037@qq.com>
2024-04-26 23:13:49 +0800

    feat: get main context

--- internal/job/fetchannounce_test.go
+++ internal/job/fetchannounce_test.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/net/html"
 	"log"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -28,4 +29,34 @@ func TestGetMainTitle(t *testing.T) {
 		log.Fatal(err)
 	}
 	fmt.Printf("Main title: %s\n", title)
+}
+
+func TestGetMainContext(t *testing.T) {
+	// 获取HTML内容
+	resp, err := http.Get("https://www.robomaster.com/zh-CN/resource/pages/announcement/1653")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// 解析HTML
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 从根节点开始查找
+	context, err := getMainContext(doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var w strings.Builder
+	err = html.Render(&w, context)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := w.String()
+	fmt.Printf("Main context: %s\n", str)
 }
