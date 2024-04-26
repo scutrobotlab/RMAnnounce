593b564d218d63f2aceb3c29ee0d8e0a34bea515
Summer-lights <1067088037@qq.com>
2024-04-27 00:13:46 +0800

    test: TestSendWebhookMsg

--- internal/util/larkwebhook_test.go
+++ internal/util/larkwebhook_test.go
@@ -13,8 +13,11 @@ func TestSendWebhookMsg(t *testing.T) {
 
 	err := SendPostMsg(c.Webhooks, "机器人碎碎念", [][]Content{
 		{
-			{Tag: "at", UserId: "ou_892a4b47aa876f799ca3aef97403e009"},
-			{Tag: "text", Text: " TEXT"},
+			// {Tag: "at", UserId: "ou_892a4b47aa876f799ca3aef97403e009"},
+			{Tag: "text", Text: "悄悄告诉大家，我刚刚学会了一个新的技能。" +
+				"现在我不仅可以实时监控 RoboMaster 资料站的新页面，还可以在旧页面《RoboMaster 2024机甲大师高校系列赛比赛规范文件》更新时通知大家哦！" +
+				"以后大家就可以第一时间获知比赛规范文件的更新啦！\n" +
+				"私有化部署的队伍可以执行 docker-compose pull && docker-compose up -d 获取最新版本哦~"},
 		},
 	})
 	if err != nil {
