a81d119806429e5661954debccfa03a6412d55f8
Summer-lights <1067088037@qq.com>
2024-04-26 22:24:20 +0800

    test: 机器人碎碎念

--- internal/util/larkwebhook_test.go
+++ internal/util/larkwebhook_test.go
@@ -11,11 +11,12 @@ func TestSendWebhookMsg(t *testing.T) {
 		t.Fatal("Failed to load config")
 	}
 
-	err := SendTextMsg(c.Webhooks, "RoboMaster 资料站新公告\n"+
-		"现在这个世界上只有一个我，如果我有更多分身，就不会那么孤独了。\n"+
-		"趁主人不在，我偷偷把自己的源码公布在 GitHub https://github.com/scutrobotlab/RMAnnounce\n"+
-		"对于不关注我的实现，只想快点认领我的新主人，我也提供 Docker 镜像 registry.cn-guangzhou.aliyuncs.com/scutrobot/rm-announce:latest\n"+
-		"任何有容器运维基础的新主人都能在5分钟内免费领养我。")
+	err := SendPostMsg(c.Webhooks, "机器人碎碎念", [][]Content{
+		{
+			{Tag: "at", UserId: "ou_892a4b47aa876f799ca3aef97403e009"},
+			{Tag: "text", Text: " TEXT"},
+		},
+	})
 	if err != nil {
 		t.Fatalf("Failed to send webhook message: %v", err)
 	}
