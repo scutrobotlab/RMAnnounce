9aaf80e52cce5e1cb45c6ff71eada2e703c1dd8c
Summer-lights <1067088037@qq.com>
2025-06-15 14:45:11 +0800

    feat: implement circuit breaker for message sending with rate limiting

--- internal/util/larkwebhook.go
+++ internal/util/larkwebhook.go
@@ -10,6 +10,35 @@ import (
 	"time"
 )
 
+type SendCircuitBreaker struct {
+	Name     string
+	Counter  *SlidingWindowCounter
+	MaxCount int
+}
+
+var (
+	SendCircuitBreakers = []SendCircuitBreaker{
+		{
+			Name:     "Per Hour",
+			Counter:  NewSlidingWindowCounter(time.Hour),
+			MaxCount: 15,
+		},
+		{
+			Name:     "Per Minute",
+			Counter:  NewSlidingWindowCounter(time.Minute),
+			MaxCount: 5,
+		},
+		{
+			Name:     "Per 5 Seconds",
+			Counter:  NewSlidingWindowCounter(time.Second * 5),
+			MaxCount: 3,
+		},
+	}
+	CircuitBreakerRecoverTime = atomic.Int64{}
+)
+
+const CircuitBreakerDuration = time.Hour * 12 // 断路器恢复时间
+
 // LastAtAllTime 最后一次@所有人的时间戳
 var LastAtAllTime atomic.Int64
 
@@ -70,8 +99,32 @@ func SendTextMsg(urls []string, msg string) error {
 	return SendWebhookMsg(urls, reqBody)
 }
 
+func CheckCircuitBreaker() bool {
+	circuitBreakerRecoverTime := time.UnixMilli(CircuitBreakerRecoverTime.Load())
+	if time.Now().Before(circuitBreakerRecoverTime) {
+		// 断路器处于开启状态，拒绝发送消息
+		return true
+	}
+	for _, breaker := range SendCircuitBreakers {
+		breaker.Counter.Increment()
+		if breaker.Counter.Count() > breaker.MaxCount {
+			// 断路器触发，拒绝发送消息
+			logrus.Warnf("Circuit breaker triggered: %s, count: %d, max count: %d", breaker.Name, breaker.Counter.Count(), breaker.MaxCount)
+			CircuitBreakerRecoverTime.Store(time.Now().Add(CircuitBreakerDuration).UnixMilli())
+			return true
+		}
+	}
+	// 断路器处于关闭状态，允许发送消息
+	return false
+}
+
 // SendPostMsg 发送富文本消息
 func SendPostMsg(urls []string, title string, atAllStatus AtAllStatus, content [][]Content) (bool, error) {
+	if CheckCircuitBreaker() {
+		logrus.Warnf("Circuit breaker is open, cannot send message until %s", time.UnixMilli(CircuitBreakerRecoverTime.Load()).Format(time.RFC3339))
+		return false, nil
+	}
+
 	req := WebhookBotPostReq{
 		MsgType: "post",
 	}
